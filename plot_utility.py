9d38c9f85381c4b9ccdf642865eddbc011d5d341
Lukas Kaleta <lukas.kvido.kaleta@seznam.cz>
2025-02-01 17:49:24 +0100

    added plot utility, renamed files

--- plot_utility.py
+++ plot_utility.py
@@ -1,49 +1,175 @@
-# select one cml:
-cml_k = '16'
-my_cml = cml_set.sel(cml_id = cml_k)        # .sel is not int indexing, but selecting specific 'label' 
-my_ref = ref_set.sel(cml_id = cml_k)
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Filename: plot_utility.py
+Author: Lukas Kaleta
+Date: 2025-01-31
+Version: 1.0
+Description: 
+    This script contains function set for using matplotlib to plot 
+    input/output/reference of CNN w/d classification using data from CML.
 
-# shaded refernece wet periods from Pycomlink
-# set first and last value with zero for correct plotting
-my_ref['ref_wd'][0] = False
-#my_ref['ref_wd'][-1] = False
+License: 
+Contact: 211312@vutbr.cz
+"""
 
-# setup figure
-fig, axs = plt.subplots(2, 1, sharex=True, figsize=(12,4))
-#ax1 = axs[0].twiny()
-#fig.tight_layout()
+""" Imports """
+# Import python libraries
 
-# plot TRSL
-my_cml.trsl.plot.line(x='time', ax=axs[0], label = 'TL');
-# plot Rain rate 
-my_ref.rain.plot.line(x='time', ax=axs[1], label = 'TL');
+import numpy as np
+import matplotlib.pyplot as plt
 
-# plot real bool wet/dry with 5min precission
-wet_start = np.roll(my_ref.ref_wd, -1) & ~my_ref.ref_wd
-wet_end = np.roll(my_ref.ref_wd, 1) & ~my_ref.ref_wd
-for wet_start_i, wet_end_i in zip(
-    wet_start.values.nonzero()[0],
-    wet_end.values.nonzero()[0],
-):
-    axs[1].axvspan(my_ref.time.values[wet_start_i], my_ref.ref_wd.time.values[wet_end_i], color='b', alpha=0.2, linewidth=0); # https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.axvspan.html
-    axs[0].axvspan(my_ref.time.values[wet_start_i], my_ref.ref_wd.time.values[wet_end_i], color='b', alpha=0.2, linewidth=0); # https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.axvspan.html
+import xarray as xr
 
-# plot trsl gaps
-my_cml['trsl_gap'][0] = False
-my_cml['trsl_gap'][-1] = False
+# Import external packages
 
-gap_start = np.roll(my_cml.trsl_gap, -1) & ~my_cml.trsl_gap
-gap_end = np.roll(my_cml.trsl_gap, 1) & ~my_cml.trsl_gap
-for gap_start_i, gap_end_i in zip(
-    gap_start.values.nonzero()[0],
-    gap_end.values.nonzero()[0],
-):
-    axs[0].axvspan(my_cml.time.values[gap_start_i], my_cml.time.values[gap_end_i], color='r', alpha=0.7, linewidth=0); # https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.axvspan.html
+# Import own modules
+
+
+""" Variable definitions """
+
+
+""" Function definitions """
+
+def plot_cnn_output(ds:xr.Dataset, cnn_wd_threshold = 0.5):
+    """
+    plot wet/dry classification output of CNN
+
+    Parameters
+    ds : xarray.dataset containing several CMLs with trsl, reference data,
+        timestamps, metadata, cnn prediction output, TP, FP, FN periods
+    cnn_wd_threshold : float, default = 0.5, classification threshold 
+        to determine boolean wet/dry flag from cnn 0-1 output
+
+    Returns: none
+    """
+    num_cmls = len(ds.cml_id)
+    n_samples = len(ds.sample_num)
+
+    # setup figure
+    fig, axs = plt.subplots(num_cmls, 1, sharex=True, figsize=(12,num_cmls*2))
+    ax1 = axs[0].twiny()
+    ax1.set_xlim(ds.time.values[0,0], ds.time.values[-1,-1])
+    fig.tight_layout(h_pad = 3)
+
+    for n in range(num_cmls):    
+        axs[n].set_xlim(0, n_samples)
+        axs[n].set_ylim(0,1.2);    
+        # plot cnn prediction
+        ds.cnn_out[n].plot.line(x='sample_num', ax=axs[n], label = 'TL',color='black', lw=0.5);
+
+        #cnn threshold
+        axs[n].axhline(cnn_wd_threshold, color='black', linestyle='--', lw=0.5)
+
+        # GREEN: plot true cnn predicted wet/dry areas
+        # tip from stack ovefrolw: https://stackoverflow.com/questions/44632903/setting-multiple-axvspan-labels-as-one-element-in-legend
+        start = np.roll(ds.true_wet[n], -1) & ~ds.true_wet[n]
+        end = np.roll(ds.true_wet[n], 1) & ~ds.true_wet[n]
+        for start_i, end_i in zip(
+            start.values.nonzero()[0],
+            end.values.nonzero()[0],
+        ):
+            axs[n].axvspan(ds.sample_num.values[start_i], ds.sample_num.values[end_i], color='g', alpha=0.5, linewidth=0, label='_'*start_i+'true wet') 
+        
+        # RED: plot false alarms
+        start = np.roll(ds.false_alarm[n], -1) & ~ds.false_alarm[n]
+        end = np.roll(ds.false_alarm[n], 1) & ~ds.false_alarm[n]
+        for start_i, end_i in zip(
+            start.values.nonzero()[0],
+            end.values.nonzero()[0],
+        ):
+            axs[n].axvspan(ds.sample_num.values[start_i], ds.sample_num.values[end_i], color='r', alpha=0.5, linewidth=0, label='_'*start_i+'false alarm') 
+            
+        # ORANGE: plot missed wet 
+        start = np.roll(ds.missed_wet[n], -1) & ~ds.missed_wet[n]
+        end = np.roll(ds.missed_wet[n], 1) & ~ds.missed_wet[n]
+        for start_i, end_i in zip(
+            start.values.nonzero()[0],
+            end.values.nonzero()[0],
+        ):
+            axs[n].axvspan(ds.sample_num.values[start_i], ds.sample_num.values[end_i], color='orange', alpha=0.5, linewidth=0, label='_'*start_i+'missed wet')
+
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # select one cml:
+# cml_k = '16'
+# my_cml = cml_set.sel(cml_id = cml_k)        # .sel is not int indexing, but selecting specific 'label' 
+# my_ref = ref_set.sel(cml_id = cml_k)
+
+# # shaded refernece wet periods from Pycomlink
+# # set first and last value with zero for correct plotting
+# my_ref['ref_wd'][0] = False
+# #my_ref['ref_wd'][-1] = False
+
+# # setup figure
+# fig, axs = plt.subplots(2, 1, sharex=True, figsize=(12,4))
+# #ax1 = axs[0].twiny()
+# #fig.tight_layout()
+
+# # plot TRSL
+# my_cml.trsl.plot.line(x='time', ax=axs[0], label = 'TL');
+# # plot Rain rate 
+# my_ref.rain.plot.line(x='time', ax=axs[1], label = 'TL');
+
+# # plot real bool wet/dry with 5min precission
+# wet_start = np.roll(my_ref.ref_wd, -1) & ~my_ref.ref_wd
+# wet_end = np.roll(my_ref.ref_wd, 1) & ~my_ref.ref_wd
+# for wet_start_i, wet_end_i in zip(
+#     wet_start.values.nonzero()[0],
+#     wet_end.values.nonzero()[0],
+# ):
+#     axs[1].axvspan(my_ref.time.values[wet_start_i], my_ref.ref_wd.time.values[wet_end_i], color='b', alpha=0.2, linewidth=0); # https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.axvspan.html
+#     axs[0].axvspan(my_ref.time.values[wet_start_i], my_ref.ref_wd.time.values[wet_end_i], color='b', alpha=0.2, linewidth=0); # https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.axvspan.html
+
+# # plot trsl gaps
+# my_cml['trsl_gap'][0] = False
+# my_cml['trsl_gap'][-1] = False
+
+# gap_start = np.roll(my_cml.trsl_gap, -1) & ~my_cml.trsl_gap
+# gap_end = np.roll(my_cml.trsl_gap, 1) & ~my_cml.trsl_gap
+# for gap_start_i, gap_end_i in zip(
+#     gap_start.values.nonzero()[0],
+#     gap_end.values.nonzero()[0],
+# ):
+#     axs[0].axvspan(my_cml.time.values[gap_start_i], my_cml.time.values[gap_end_i], color='r', alpha=0.7, linewidth=0); # https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.axvspan.html
    
 
-# axes limits source: https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.set_xlim.html
-axs[1].set_xlim(my_cml.time.values[0], my_cml.time.values[-1])
-axs[0].set_xlabel('')
-axs[1].set_title("")
+# # axes limits source: https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.set_xlim.html
+# axs[1].set_xlim(my_cml.time.values[0], my_cml.time.values[-1])
+# axs[0].set_xlabel('')
+# axs[1].set_title("")
 
-fig.savefig('cml_aligned_ds.svg')
+# fig.savefig('cml_aligned_ds.svg')
